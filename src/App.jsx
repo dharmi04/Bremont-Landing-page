95bed6ae0a30760fdae95b92d3cd37f6eafb450e
dharmi04 <dharmip060@gmail.com>
2025-04-03 17:53:25 +0530

    Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import EventsPage from "./Pages/EventsPage";
 import Home from "./Pages/Home";
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
+    <>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/events" element={<EventsPage/>} /> */}
       </Routes>
     </Router>
+    <Analytics />
+    </>
+    
   );
 };
 
